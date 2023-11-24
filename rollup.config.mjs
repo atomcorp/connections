1bab0af887dff6587eccad02afc421ac1d2dda82
atomcorp <thomasmaxwellsmith@gmail.com>
2023-11-24 15:15:51 +0000

    try and add type def files

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,20 +2,28 @@ import typescript from "@rollup/plugin-typescript";
 import resolve from "@rollup/plugin-node-resolve";
 import postcss from "rollup-plugin-postcss";
 import terser from "@rollup/plugin-terser";
+import dts from "rollup-plugin-dts";
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default {
-  input: "./components/Game.tsx",
-  output: {
-    file: "./bundle/bundle.js",
-    format: "esm",
-    sourcemap: true,
+export default [
+  {
+    input: "./components/Game.tsx",
+    output: {
+      file: "./bundle/index.js",
+      format: "esm",
+      sourcemap: true,
+    },
+    plugins: [
+      resolve(),
+      typescript({ tsconfig: "./tsconfig.build.json" }),
+      postcss(),
+      terser(),
+    ],
+    external: ["react", "react-dom", "framer-motion"],
   },
-  plugins: [
-    resolve(),
-    typescript({ tsconfig: "./tsconfig.build.json" }),
-    postcss(),
-    terser(),
-  ],
-  external: ["react", "react-dom", "framer-motion"],
-};
+  // {
+  //   input: "./bundle/dts/Game.d.ts",
+  //   output: [{ file: "bundle/index.d.ts", format: "es" }],
+  //   plugins: [dts()],
+  // },
+];
