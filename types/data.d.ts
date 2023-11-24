57491ba953da57d9be5e4d9b973fe7bd1853e98c
atomcorp <thomasmaxwellsmith@gmail.com>
2023-11-24 14:20:26 +0000

    npm publish

--- types/data.d.ts
+++
@@ -1,9 +0,0 @@
-export declare const answers: {
-    words: string[];
-    answer: string;
-}[];
-export type Board = {
-    connection: string;
-    words: string[];
-}[];
-export declare const startingBoard: Board;
